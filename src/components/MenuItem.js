93b7f32f9dd70be48820c0d86a326cbe2f51be62
Yared <yaredyh11@gmail.com>
2025-05-13 15:28:04 +0300

    some improvment

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -7,18 +7,18 @@ function MenuItem({ item }) {
   if (!item.visible) return null;
 
   return (
-    <a href="#" class="menu-card" data-category="{{ $item->category }}">                       
+    <a href="/" className="menu-card">                       
 
         <div className='content'>
             <div className='content-img'>
                 <img src={item.image} alt={item.name} />
             </div>
-            <div class="content-txt">
+            <div className="content-txt">
                 <h3>{ item.name }</h3>
                 <p>{ item.ingredients }</p>
             </div>
         </div>
-        <span class="menu-price">{ item.price } Birr</span>
+        <span className="menu-price">{ item.price } Birr</span>
     </a>
   );
 }
