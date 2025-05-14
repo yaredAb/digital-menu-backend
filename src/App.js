aeaae552f833abc0798f229f8b263776003105fb
Yared <yaredyh11@gmail.com>
2025-05-14 08:08:15 +0300

    some improvments

--- src/App.js
+++ src/App.js
@@ -5,26 +5,6 @@ import './App.css';
 import AddMenuItem from './components/AddMenuItem';
 
 function App() {
-  const [menuItems, setMenuItems] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('');
-
-  useEffect(() => {
-    fetch('https://digital-menu-1-3i80.onrender.com/api/menu')
-      .then(res => res.json())
-      .then(data => setMenuItems(data))
-      .catch(err => console.error('Failed to fetch menu:', err));
-
-
-      //fetch categories
-      fetch('https://digital-menu-1-3i80.onrender.com/api/category')
-        .then(res => res.json())
-        .then(data => setCategories(data))
-        .catch(err =>console.error('failed to fetch category', err))
-  }, []);
-
-  const filterMenuItems = selectedCategory ? menuItems.filter(item => item.category === selectedCategory) : menuItems
-
   return (
     <Router>
       <Routes>
--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -2,6 +2,29 @@ import React from 'react'
 import MenuItem from '../components/MenuItem'
 
 export default function MenuPage() {
+
+  const [menuItems, setMenuItems] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  useEffect(() => {
+    fetch('https://digital-menu-1-3i80.onrender.com/api/menu')
+      .then(res => res.json())
+      .then(data => setMenuItems(data))
+      .catch(err => console.error('Failed to fetch menu:', err));
+
+
+      //fetch categories
+      fetch('https://digital-menu-1-3i80.onrender.com/api/category')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+        .catch(err =>console.error('failed to fetch category', err))
+  }, []);
+
+  const filterMenuItems = selectedCategory ? menuItems.filter(item => item.category === selectedCategory) : menuItems
+
+
+
   return (
     <div className="page-wrapper">
       <a className="logo">Meskott</a>
