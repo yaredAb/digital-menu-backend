fc1a39562f9882b3f28d716938ec60049460f307
Yared <yaredyh11@gmail.com>
2025-05-15 08:12:22 +0300

    adding loading spineer

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -11,25 +11,11 @@ export default function MenuPage() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    fetch('https://digital-menu-1-3i80.onrender.com/api/menu')
-      .then(res => res.json())
-      .then(data => setMenuItems(data))
-      .catch(err => console.error('Failed to fetch menu:', err));
-
-
-      //fetch categories
-      fetch('https://digital-menu-1-3i80.onrender.com/api/category')
-        .then(res => res.json())
-        .then(data => setCategories(data))
-        .catch(err =>console.error('failed to fetch category', err))
-  }, []);
-
-  useEffect(() => {
     const fetchData = async () => {
       try{
         setLoading(true)
         const [menuRes, categoryRes] = await Promise.all([
-          fetch['https://digital-menu-1-3i80.onrender.com/api/menu'],
+          fetch('https://digital-menu-1-3i80.onrender.com/api/menu'),
           fetch('https://digital-menu-1-3i80.onrender.com/api/category')
         ]);
 
